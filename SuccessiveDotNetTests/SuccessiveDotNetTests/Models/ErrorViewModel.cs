248475b5c5ecbb728700f218ed5976177bd7acad
akashsuccessive <akash@successive.tech>
2020-04-28 13:25:11 +0530

    project setup

---
+++ SuccessiveDotNetTests/SuccessiveDotNetTests/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuccessiveDotNetTests.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
